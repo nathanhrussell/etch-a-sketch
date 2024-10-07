93dd959a2554cde6b4ffccd9af126e7586ad84d4
nathanhrussell <173242953+nathanhrussell@users.noreply.github.com>
2024-10-07 12:39:23 +0200

    Refactor code to allow for createGrid function to work with different numbers

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const container = document.getElementById("container");
 
 function createGrid(rows, cols) {
+    container.style.setProperty("--grid-rows", rows);
+    container.style.setProperty("--grid-cols", cols);
+    
+    container.innerHTML = ""; 
+
     for (let i = 0; i < rows * cols; i++) {
         const div = document.createElement("div");
         div.classList.add("grid-item");
